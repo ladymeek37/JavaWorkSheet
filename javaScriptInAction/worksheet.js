472fd7daf1cac6c0e81d257aec5cd485d349cbe3
ladymeek37 <116923082+ladymeek37@users.noreply.github.com>
2023-01-10 11:02:11 -0800

    Add files via upload

---
+++ javaScriptInAction/worksheet.js
@@ -0,0 +1,132 @@
+"use strict";
+
+// //Variables Task 1
+
+// let dayOfWeek = "Monday";
+
+// console.log(dayOfWeek)
+
+// dayOfWeek = "Friday"
+
+// if (dayOfWeek === "Friday"){
+//     console.log("I can't wait for Friday!")
+// }
+
+
+// //Variables Task 2
+
+// let animalInput =
+//         prompt(
+//             "What is your favorite aminal?"
+//         );
+
+ 
+// let colorInput =
+//     prompt(
+//         "What is your favorite color?"
+//     );
+
+// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+
+// //Conditionals Task 1
+
+// let timeOfDay = 1000;
+// let randomMeal = "Meal";
+
+// if (timeOfDay < 1200){
+//     randomMeal = "Toasted Bagel with Avocado";
+// } else if (timeOfDay > 1200 && timeOfDay < 1700){
+//     randomMeal = "Acai Bowl";
+// } else if (timeOfDay >= 1700){
+//     randomMeal = "Pizza";
+// }
+
+// console.log(randomMeal);
+
+
+//Conditionals Task 2
+
+// let randomNum = Math.floor(Math.random() * 11);
+
+// if (randomNum >= 0 && randomNum <= 2){
+//     console.log("Beatles");
+// } else if (randomNum >= 3 && randomNum <= 5){
+//     console.log("Stones");
+// } else if (randomNum >= 6 && randomNum <= 8){
+//     console.log("Floyd");
+// } else if (randomNum === 9 || randomNum ===10){
+//     console.log("Hendrix")
+// }
+
+//For Loops Task 1
+
+// let count = 0;
+// let message = "JavaScript is cool!"
+
+// while (count < 7){
+//     console.log(message)
+//     count ++;
+// }
+
+
+//For Loops Task 2
+
+// let number = 0 
+
+// while(number < 11){
+//     console.log(number)
+//     number ++;
+// }
+
+// For Loops Task 3
+
+// let count = 0
+
+// while (count < 5){
+//     count += 1
+//     console.log("hello");
+//     console.log("goodbye");
+// }
+
+
+// Functions Task 1
+
+// function printFavMovie (){
+//     let favMovie = "Nemo"
+//     console.log(favMovie)
+// }
+
+// let myFavMovie = printFavMovie()
+
+//Functions Task 2
+
+// function favBand (){
+//     let question = prompt("What is your favorite band?")
+//     return question
+// }
+
+// let yourFavBand = favBand()
+// console.log(`Your favorite band is ${yourFavBand}.`)
+
+// // Functions Task 3
+
+// function concertDisplay(musicalAct){
+//     let myStreet = prompt("What street do you live on?")
+//     console.log(`It would be great if ${musicalAct} played a show on ${myStreet}!`)
+//     return myStreet
+// }
+
+// concertDisplay(yourFavBand)
+
+// // Arrays Task 1
+
+//  let desktopItems = ["Pens", "Candle", "Coffee Cup", "Journal"]
+//  console.log(desktopItems[1])
+
+//  desktopItems.push("Infinity Gauntlet")
+
+//  for ( let i=0; i < desktopItems.length; i++){
+//     console.log(desktopItems[i])
+//  }
+
